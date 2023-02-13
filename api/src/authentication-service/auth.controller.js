14bd9899dad43bdbde66e27515892261794bb1bd
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-02-13 17:23:49 +0100

    login & register function done

--- api/src/authentication-service/auth.controller.js
+++ api/src/authentication-service/auth.controller.js
@@ -4,10 +4,37 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const Login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
   try {
-  } catch (error) {}
+    if (!email || !password) {
+      res.status(400);
+      throw new Error("Please fill in all fields");
+    }
+    const oldUser = await User.findOne({ email });
+
+    if (!oldUser)
+      return res.status(404).json({ message: "User doesn't exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { email: oldUser.email, id: oldUser._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(200).json({ result: oldUser, token });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 });
 
+// create a new user
 const Register = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
@@ -40,13 +67,5 @@ const Register = asyncHandler(async (req, res) => {
     throw new Error("Invalid user data");
   }
 });
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
-};
-
-// create a new user 
 
 module.exports = { Login, Register };
