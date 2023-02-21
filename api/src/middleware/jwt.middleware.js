9b61958ae5c2bb17c3c69626afea954dd37f905e
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-02-21 19:04:02 +0100

    jwt middleware

---
+++ api/src/middleware/jwt.middleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const isCustomAuth = token.length < 500;
+
+    let decodedData;
+
+    if (token && isCustomAuth) {
+      decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.userId = decodedData?.id;
+    } else {
+      decodedData = jwt.decode(token);
+
+      req.userId = decodedData?.sub;
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { auth };
