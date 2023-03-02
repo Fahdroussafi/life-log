cd4d6773399744951a7b30903cb5298a69c78145
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-03-02 11:57:07 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        avenir: ['Avenir', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
