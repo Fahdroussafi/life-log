37a8975257b42eb942a286d56155b812b450575d
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-03-02 13:33:06 +0100

    Update LandingScreen.jsx

--- frontend/screens/LandingScreen.jsx
+++ frontend/screens/LandingScreen.jsx
@@ -1,41 +1,44 @@
-import React from "react";
+import React from 'react';
 import {
   View,
   Text,
   ImageBackground,
   TouchableOpacity,
   SafeAreaView,
-} from "react-native";
+} from 'react-native';
 
-const wallpaper = require("../assets/wallpaper.png");
+const wallpaper = require('../assets/wallpaper.png');
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = ({navigation}) => {
   return (
     <SafeAreaView>
       <ImageBackground
         source={wallpaper}
         className="w-full h-full object-cover"
         resizeMode="cover">
-        <TouchableOpacity onPress={() => navigation.navigate("Home")}>
-          <Text className="text-[#000000] tracking-normal font-bold right-10 text-right text-2xl py-10 ">
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Home')}>
+          <Text className="text-[#000000] font-[avenir] tracking-normal font-bold right-10 text-right text-2xl py-10 ">
             Skip
           </Text>
         </TouchableOpacity>
         <View className="flex-1 pl-7">
-          <Text className="text-[#000000] left-10 text-left text-2xl mb-3 tracking-normal">
+          <Text className="text-[#000000] font-[avenir] left-16 text-left text-2xl mb-3 tracking-normal">
             It's a Big World
           </Text>
-          <Text className="text-[#000000] text-center text-6xl font-extrabold tracking-widest">
+          <Text className="text-[#000000] font-[avenir] text-center text-5xl font-extrabold tracking-widest">
             Out There,
           </Text>
-          <Text className="text-[#000000] text-center text-6xl font-extrabold tracking-widest">
+          <Text className="text-[#000000] font-[avenir] text-center text-5xl font-extrabold tracking-widest">
             Go Explore
           </Text>
 
-          <View className="mt-[500px] items-center">
-            <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-              <Text className="text-[#FFFFFF] font-bold tracking-widest text-base bg-[#20B08E] p-3 rounded-full text-center w-60 ">
-                LOGIN
+          <View className="mt-96 items-center">
+            <TouchableOpacity
+              className="w-60 bg-[#20B08E] p-3 rounded-full"
+              onPress={() => navigation.navigate('Auth', {screen: 'Login'})}>
+              <Text className="text-[#FFFFFF] font-[avenir] font-bold tracking-widest text-base text-center ">
+                GET STARTED
               </Text>
             </TouchableOpacity>
           </View>
