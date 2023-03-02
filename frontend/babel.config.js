d98acc624cec90459ec3f0ddee831cb761e62581
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-03-02 11:57:50 +0100

    native wind & animation config

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['nativewind/babel'],
+  plugins: [
+    'nativewind/babel',
+    ['react-native-reanimated/plugin', {relativeSourceLocation: true}],
+  ],
 };
