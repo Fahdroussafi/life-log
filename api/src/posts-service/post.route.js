c8255a3208db2145dd41e95dddb5d14fc32bbb2e
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-03-09 11:23:43 +0100

    searching route added

--- api/src/posts-service/post.route.js
+++ api/src/posts-service/post.route.js
@@ -6,14 +6,23 @@ const {
   DeletePost,
   GetPosts,
   LikePost,
+  CommentPost,
+  getPostsBySearch,
+  getPostsByCreator,
+  getPost,
 } = require("../posts-service/post.controller");
 
 const { auth } = require("../middleware/jwt.middleware");
 
-router.post("/createpost", CreatePost);
-router.get("/getposts", GetPosts);
+router.post("/createPost", CreatePost);
+router.get("/getPosts", GetPosts);
 router.patch("/:id", UpdatePost);
 router.delete("/:id", DeletePost);
-router.patch("/:id/likepost", auth, LikePost);
+router.patch("/:id/likePost", auth, LikePost);
+
+// router.get('/:id', getPost);
+// router.get("/creator", getPostsByCreator);
+router.get("/search", getPostsBySearch);
+router.post('/:id/commentPost', CommentPost);
 
 module.exports = router;
