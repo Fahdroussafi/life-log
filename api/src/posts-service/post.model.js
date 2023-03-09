9ebc9dbe4cae5d9f60566b13dc5a388f1b58f46b
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-03-09 11:19:03 +0100

    added timestaps

--- api/src/posts-service/post.model.js
+++ api/src/posts-service/post.model.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
-const postSchema = new mongoose.Schema({
-  title: { type: String },
-  message: { type: String },
-  name: { type: String },
-  creator: { type: String },
-  tags: { type: [String] },
-  selectedFile: { type: String },
-  likes: { type: [String], default: [] },
-  comments: { type: [String], default: [] },
-  createdAt: {
-    type: Date,
-    default: new Date(),
+const postSchema = new mongoose.Schema(
+  {
+    title: { type: String },
+    message: { type: String },
+    name: { type: String },
+    creator: { type: String },
+    tags: { type: [String] },
+    selectedFile: { type: String },
+    likes: { type: [String], default: [] },
+    comments: { type: [String], default: [] },
+    // createdAt: {
+    //   type: Date,
+    //   default: new Date(),
+    // },
   },
-});
+  { timestamps: true }
+);
 
 postSchema.plugin(uniqueValidator);
 module.exports = mongoose.model("Posts", postSchema);
