fc514156ecd54b701cb9ccf54da769008d51e3d4
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-03-28 00:41:49 +0000

    added requirement true

--- api/src/posts-service/post.model.js
+++ api/src/posts-service/post.model.js
@@ -3,18 +3,18 @@ const uniqueValidator = require("mongoose-unique-validator");
 
 const postSchema = new mongoose.Schema(
   {
-    title: { type: String },
-    message: { type: String },
+    title: { type: String, required: true },
+    message: { type: String, required: true },
     name: { type: String },
-    creator: { type: String },
-    tags: { type: [String] },
-    selectedFile: { type: String },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    tags: { type: [String], required: true },
+    selectedFile: { type: String, required: true },
     likes: { type: [String], default: [] },
     comments: { type: [String], default: [] },
-    // createdAt: {
-    //   type: Date,
-    //   default: new Date(),
-    // },
   },
   { timestamps: true }
 );
