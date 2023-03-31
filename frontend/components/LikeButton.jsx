f193ec838703888ec7fb98629c0d92c9d4bc5db2
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-03-31 02:16:13 +0000

    like button

---
+++ frontend/components/LikeButton.jsx
@@ -0,0 +1,117 @@
+import React, {useState, useEffect, useContext} from 'react';
+import {TouchableOpacity, View, Text} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialIcons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import {CredentialsContext} from './../components/CredentialsContext';
+import {useNavigation} from '@react-navigation/native';
+
+const LikeButton = ({postId}) => {
+  const navigation = useNavigation();
+  const [liked, setLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(0);
+  const baseUrl = 'http://192.168.1.3:8080';
+
+  const {storedCredentials, setStoredCredentials} =
+    useContext(CredentialsContext);
+
+  useEffect(() => {
+    // Check if the post is liked by the user
+    const checkLiked = async () => {
+      try {
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+          const res = await axios.get(
+            `${baseUrl}/api/posts/${postId}/checkIfLiked`,
+            {
+              headers: {Authorization: `Bearer ${token}`},
+            },
+          );
+          setLiked(res.data.isLiked);
+        } else {
+          setLiked(false);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    checkLiked();
+
+    // Get the total number of likes for the post
+    const getLikesCount = async () => {
+      try {
+        const res = await axios.get(
+          `${baseUrl}/api/posts/${postId}/likesCount`,
+        );
+        setLikesCount(res.data.likesCount);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getLikesCount();
+
+    // Reset state when credentials are cleared
+    if (!storedCredentials) {
+      setLiked(false);
+      setLikesCount(0);
+    }
+  }, [postId, storedCredentials]);
+
+  const handleLike = async () => {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        const res = await axios.patch(
+          `${baseUrl}/api/posts/${postId}/likePost`,
+          {},
+          {headers: {Authorization: `Bearer ${token}`}},
+        );
+        setLiked(res.data.liked);
+        setLikesCount(res.data.updatedPost.likes.length);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      const checkLiked = async () => {
+        try {
+          const token = await AsyncStorage.getItem('token');
+          if (token) {
+            const res = await axios.get(
+              `${baseUrl}/api/posts/${postId}/checkIfLiked`,
+              {
+                headers: {Authorization: `Bearer ${token}`},
+              },
+            );
+            setLiked(res.data.isLiked);
+          } else {
+            setLiked(false);
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      checkLiked();
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  return (
+    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+      <TouchableOpacity onPress={handleLike}>
+        <MaterialCommunityIcons
+          name={liked ? 'favorite' : 'favorite-border'}
+          size={24}
+          color={liked ? 'red' : 'black'}
+        />
+      </TouchableOpacity>
+      <Text style={{marginLeft: 5, color: 'black'}}>{likesCount}</Text>
+    </View>
+  );
+};
+
+export default LikeButton;
