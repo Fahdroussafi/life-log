387e8293938f0aff89e807a33785071bc1d87bd6
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-04-03 00:13:23 +0000

    UI change

--- frontend/screens/HomeScreen.jsx
+++ frontend/screens/HomeScreen.jsx
@@ -20,16 +20,20 @@ const HomeScreen = () => {
   };
 
   return (
-    <SafeAreaView className="flex-1 bg-[#242424] relative ">
-      <View className="p-4 ">
-        <View className="flex flex-row justify-between items-center">
-          <Text className="text-white text-2xl py-5 tracking-widest font-[avenir] font-bold">
-            Hi{' '}
-            <Text className="text-white font-bold text-2xl font-[avenir]">
-              {storedCredentials.userName
-                ? storedCredentials.userName
-                : 'Guest'}
-            </Text>
+    <SafeAreaView className="flex-1 bg-[#242424] ">
+      <View className="p-4">
+        <View className="flex flex-row justify-between items-center py-2">
+          <Text className="text-white text-2xl tracking-widest font-[avenir] font-bold">
+            Hi,{' '}
+            {storedCredentials.userName ? (
+              <Text className="text-[#20B08E] font-bold text-2xl font-[avenir]">
+                {storedCredentials.userName}
+              </Text>
+            ) : (
+              <Text className="text-white font-bold text-2xl font-[avenir]">
+                Guest
+              </Text>
+            )}
           </Text>
           {storedCredentials.userName && (
             <TouchableOpacity
@@ -41,9 +45,6 @@ const HomeScreen = () => {
             </TouchableOpacity>
           )}
         </View>
-        <Text className="text-white font-extrabold tracking-widest text-2xl font-[avenir]">
-          Where do you want to go ?
-        </Text>
       </View>
       <Posts />
     </SafeAreaView>
