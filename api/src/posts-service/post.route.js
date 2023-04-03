93ddefa4f069474c26470a2b496e6399c9491055
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-04-03 13:40:19 +0000

    Update post.route.js

--- api/src/posts-service/post.route.js
+++ api/src/posts-service/post.route.js
@@ -18,15 +18,16 @@ const { auth } = require("../middleware/jwt.middleware");
 
 router.post("/createPost", auth, CreatePost);
 router.get("/search", getPostsBySearch);
+router.get("/:id/likesCount", getLikeCount);
 router.get("/getPosts", GetPosts);
-router.patch("/:id", UpdatePost);
-router.delete("/:id", DeletePost);
 router.patch("/:id/likePost", auth, LikePost);
+// router.patch("/:id", UpdatePost);
+// router.delete("/:id/", DeletePost);
+router.get("/:id/checkIfLiked", auth, checkIfLiked);
 
-router.get("/:id", getPost);
+
 // router.get("/creator", getPostsByCreator);
 router.post("/:id/commentPost", CommentPost);
-router.get("/:id/likesCount", getLikeCount);
-router.get("/:id/checkIfLiked", auth, checkIfLiked);
+router.get("/:id", getPost);
 
 module.exports = router;
