fb5da5c45c6cddd8ca289882e162d114841d3c66
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-04-05 19:55:30 +0000

    Update HomeStack.js

--- frontend/navigators/HomeStack.js
+++ frontend/navigators/HomeStack.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import HomeScreen from '../screens/HomeScreen';
-import PostScreen from '../screens/PostScreen';
+import PostScreen from '../screens/PostScreen/PostScreen';
 import CustomHeader from '../components/CustomHeader';
 
 const HomeStack = createNativeStackNavigator();
